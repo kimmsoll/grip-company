2a4d796529ce342bdff348fef6867d31d7da36d4
kimmsoll <kimmsoll@naver.com>
2022-05-13 15:57:18 +0900

    feat: create Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import styles from './Loading.module.scss'
+
+const Loading = () => {
+  return (
+    <div className={styles.loadingContainer}>
+      <p>Loading</p>
+    </div>
+  )
+}
+
+export default Loading
